4433a399bb6a8e3b2e22ba392a2b25e88117f9ae
Matthias <matthias.wagner.office@googlemail.com>
2013-06-14 18:40:59 -0600

    add get_retina_url helper method
    So you can get the retina url for css background images.
    
    ex:
    .school-hero-unit {
        background-image: url('#{image.url(:banner)}');
      }
    
      @media
      (-webkit-min-device-pixel-ratio: 2),
      (min-resolution: 192dpi) {
        .school-hero-unit-bg {
          background-image: url('#{get_retina_url(image.url(:banner))}');
        }
      }

--- lib/retina_rails/helpers.rb
+++ lib/retina_rails/helpers.rb
@@ -19,6 +19,14 @@ module ActionView
       end
       alias_method_chain :image_tag, :retina
 
+      def get_retina_url(source)
+        retina_source = source.to_s
+        retina_source = retina_source.split('.')
+        filename      = retina_source.slice!(-2)
+        retina_source = retina_source.insert(-2, "#{filename}@2x").join('.')
+
+        path_to_image(retina_source)
+      end
     end
   end
 end
