4b02f978ca327da2381ff1770e7b5845f9ba1fa2
Johan van Zonneveld <johan@vzonneveld.nl>
2013-10-18 22:31:50 +0200

    Remove unused asset configuration

--- spec/support/rails.rb
+++ spec/support/rails.rb
@@ -6,17 +6,6 @@ module RetinaRailsTest
   class Application < Rails::Application
     config.active_support.deprecation = :log
 
-    config.assets.manifest = Rails.root.join("spec/fixtures")
-
-    ## Asset config
-
-    config.assets.version      = '1.0'
-    config.serve_static_assets = false
-    config.assets.enabled      = true
-    config.assets.compress     = true
-    config.assets.compile      = false
-    config.assets.digest       = true
-
     if Rails::VERSION::STRING >= "4.0.0"
       config.secret_token    = 'existing secret token'
       config.secret_key_base = 'new secret key base'
