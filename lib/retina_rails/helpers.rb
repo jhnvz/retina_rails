5c97888db9c23e2744f735977858fd1745daefb3
Johan van Zonneveld <johan@vzonneveld.nl>
2013-10-18 22:34:42 +0200

    Better readable code

--- lib/retina_rails/helpers.rb
+++ lib/retina_rails/helpers.rb
@@ -8,7 +8,7 @@ module ActionView
       # === Parameters
       #
       # [model (Model)] model instance
-      # [mounted_as (Sym)] attribute to which uploader is mounted
+      # [mounted_to (Sym)] attribute to which uploader is mounted
       # [version (Sym)] version of the upload
       # [options (Hash)] optional options hash
       #
@@ -16,11 +16,11 @@ module ActionView
       #
       # retina_image_tag(@user, :avatar, :small)
       #
-      def retina_image_tag(model, mounted_as, version, options={})
-        dimensions = model.retina_dimensions[mounted_as.to_sym][version.to_sym]
+      def retina_image_tag(model, mounted_to, version, options={})
+        dimensions = model.retina_dimensions[mounted_to.to_sym][version.to_sym]
         options    = dimensions.merge(options)
 
-        image_tag(model.send(mounted_as).url(version), options)
+        image_tag(model.send(mounted_to).url(version), options)
       end
 
     end
