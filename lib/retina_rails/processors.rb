8e52fcb3d6201709050ac3e7412ad1cb1e5ce359
Johan van Zonneveld <johan@vzonneveld.nl>
2014-03-27 11:12:58 +0100

    Refactor

--- lib/retina_rails/processors.rb
+++ lib/retina_rails/processors.rb
@@ -1,2 +1,2 @@
-require 'retina_rails/processors/paperclip' if defined?(::Paperclip)
 require 'retina_rails/processors/carrierwave' if defined?(::CarrierWave)
+require 'retina_rails/processors/paperclip' if defined?(::Paperclip)
