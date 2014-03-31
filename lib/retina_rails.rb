40ef1e59853b7192c5aaf0d53404d7e56d8c19dc
Johan van Zonneveld <johan@vzonneveld.nl>
2014-03-31 10:59:25 +0200

    Change load order

--- lib/retina_rails.rb
+++ lib/retina_rails.rb
@@ -8,16 +8,16 @@ module RetinaRails
 
   if defined?(Rails)
     class Railtie < Rails::Railtie
-      initializer "retina_rails.include_strategies" do
-        RetinaRails::Strategies.include_strategies
-      end
-
       initializer "retina_rails.include_extenions" do
         RetinaRails::Extensions.include_extensions
       end
 
       initializer "retina_rails.include_processors" do
         RetinaRails::Processors.include_processors
+      end
+
+      initializer "retina_rails.include_strategies" do
+        RetinaRails::Strategies.include_strategies
       end
     end
   end
